fbad6a56040735216b04750d29112b7d586c80be
parry <parry@parrys-air.attlocal.net>
2016-11-05 18:27:10 -0700

    styling profile view

---
+++ TwitterRedux/TFProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TFProfileViewController.swift
+//  TwitterRedux
+//
+//  Created by parry on 11/5/16.
+//  Copyright © 2016 parry. All rights reserved.
+//
+
+import UIKit
+
+class TFProfileViewController: UIViewController {
+
+    @IBOutlet weak var tweetsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+
+}
