3253b8cae883116db8cea75096fb3f2075f55241
parry <parry@parrys-air.attlocal.net>
2016-11-06 18:35:25 -0800

    timeline starting

--- TwitterRedux/TRProfileViewController.swift
+++ TwitterRedux/TRProfileViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-let kTweetsTableViewCellIdentifier = "TweetTableViewCell"
+let kTweetsTableViewCellIdentifier = "TRTweetTableViewCell"
 
 class TRProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
