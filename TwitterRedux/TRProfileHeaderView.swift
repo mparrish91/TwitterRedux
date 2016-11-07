c40ff0d3163bfa5cc780038970814ec5ef6bcccb
parry <parry@parrys-air.attlocal.net>
2016-11-07 12:02:53 -0800

    created header xib

---
+++ TwitterRedux/TRProfileHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  TRProfileHeaderView.swift
+//  TwitterRedux
+//
+//  Created by parry on 11/7/16.
+//  Copyright © 2016 parry. All rights reserved.
+//
+
+import UIKit
+
+class TRProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
